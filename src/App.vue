5950126a6964544cbfe8e2717d7ad4aceb2cd0bf
Casey Li <casey.li@gmail.com>
2020-06-11 22:05:55 -0400

    Initial commit.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,80 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>Count: {{count}}</div>
+    <button @click.prevent='increment'>Increment</button>
+
+    <h1>Products</h1>
+
+    <div v-if='!loading' class='products'>
+      <Product 
+        v-for='product in products' 
+        :key='product.name' 
+        :product='product'
+        @select='handleProductClick' />
+    </div>
+    <div v-else>
+      Loading products...
+    </div>
+
+    <h1>New Product</h1>
+
+    <form @submit.prevent='handleNewProductSubmit'>
+      <div class="field">
+        <label>Name</label>
+        <input type="text" name="name" v-model='newProduct.name' />
+      </div>
+      <div class="field">
+        <label>Price</label>
+        <input type="text" name="price" v-model.number='newProduct.price' />
+      </div>
+      <div class="actions">
+        <input type='submit' />
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Product from './components/Product.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Product
+  },
+  data() {
+    return {
+      count: 0,
+      showDetails: false,
+      loading: true,
+      products: [],
+      newProduct: {
+        name: '',
+        price: 0.00,
+      },
+    };
+  },
+  created() {
+    this.products = [
+      { name: "Macbook Pro", price: 2399 },
+      { name: "Macbook Air", price: 999 },
+    ];
+    this.loading = false;
+  },
+  methods: {
+    increment() {
+      this.count++;
+    },
+    toggleDetails() {
+      this.showDetails = !this.showDetails
+    },
+    handleNewProductSubmit() {
+      this.products.push(this.newProduct);
+      this.newProduct = { name: '', price: 0.00 };
+    },
+    handleProductClick(product) {
+      alert(`${product.name} clicked!`);
+    }
   }
 }
 </script>
---
+++ src/components/Product.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='product' @click='$emit("select", product)'>
+    <div class='name'>{{product.name}}</div>
+    <div class='price'>${{product.price}}</div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['product']
+}
+</script>
+
+<style>
+  .product {
+    border: 1px solid #eee;
+    background: #fefefe;
+    padding: 20px;
+    margin: 10px 0;
+  }
+</style>
